142864855fce64094297c778787a711ae02abd53
Dominique DEBERT <dominiquedebert@gmail.com>
2025-09-17 16:44:13 +0200

    feat/Homepage : logo and background

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
-import "../globals.css";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
+
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-bold text-primary">
-        Bonjour
-      </Text>
-      <Text className="text-5xl font-bold text-secondary mt-5">
-         Dominique!
-      </Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView className="flex-1 px-5">
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 }
