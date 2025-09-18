0ed3016b46f51a15ce75461d7797f581004532d0
Dominique DEBERT <dominiquedebert@gmail.com>
2025-09-18 14:52:55 +0200

    feat/SearchBar implementation - follow up

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,13 +1,23 @@
-import {View, Text, Image, TextInput} from 'react-native'
-import React from 'react'
-import {icons} from "@/constants/icons";
+import { icons } from "@/constants/icons";
+import React from 'react';
+import { Image, TextInput, View } from 'react-native';
 
-const SearchBar = () => {
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
     return (
         <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
             <Image source={icons.search} className="size-5" resizeMode="contain" tintColor="#ab8bff" />
             <TextInput
-
+              onPress={onPress}
+              placeholder={placeholder}
+              value=""
+              onChangeText={() => {}}
+              placeholderTextColor={"#ab8bff"}
+              className="flex-1 ml-2 text-white"
             />
         </View>
     )
