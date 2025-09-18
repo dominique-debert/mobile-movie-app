0ae16e6af8feb7919c18a9e83a68019627806ebe
Dominique DEBERT <dominiquedebert@gmail.com>
2025-09-18 16:25:35 +0200

    feat/add useFetch Hook

--- services/api.ts
+++ services/api.ts
@@ -10,9 +10,9 @@ export const TMDB_CONFIG = {
 
 export const fetchMovies = async ({ query }: { query: string }) => {
   const endpoint = query
-    ? `/search/movie?query={encodeURIComponent(query)}`
-    : '/discover/movie?sort_by=popularity.desc'
-  
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`
+
   const response = await fetch (endpoint, {
     method: 'GET',
     headers: TMDB_CONFIG.headers
